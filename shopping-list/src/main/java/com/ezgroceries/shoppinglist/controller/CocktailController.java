1ed621ccce7ec51ce5a2816fc7a8158977a54f88
jd07987 <kenneth.piron@kbc.be>
2019-05-10 11:27:30 +0200

    add (unfinished lab4)

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/CocktailController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/CocktailController.java
@@ -6,18 +6,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 import java.util.UUID;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import io.micrometer.core.instrument.util.StringUtils;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.ezgroceries.shoppinglist.database.CocktailDBClient;
+import com.ezgroceries.shoppinglist.database.CocktailDBResponse;
+
 
 @RestController
 @RequestMapping(value = "/cocktails", produces = "application/json")
 public class CocktailController {
+    private final CocktailDBClient cocktailDBClient;
+
+    public CocktailController(CocktailDBClient cocktailDBClient) {
+        this.cocktailDBClient = cocktailDBClient;
+    }
+
 
     @GetMapping
     public List<CocktailResource> get(@RequestParam String search) {
-        return new ArrayList<>(getDummyResources());
+
+        //return new ArrayList<>(getDummyResources());
+        //return ResponseEntity.ok(generateList(cocktailDBClient.searchCocktails(search)));
+        return generateList(cocktailDBClient.searchCocktails(search));
+
     }
 
     private List<CocktailResource> getDummyResources() {
@@ -35,4 +54,32 @@ public class CocktailController {
                         "https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg",
                         Arrays.asList("Tequila", "Blue Curacao", "Lime juice", "Salt")));
     }
+
+    private List<CocktailResource> generateList(CocktailDBResponse response){
+        List<CocktailResource> listResponse;
+
+        // map the list<DrinkResource> to the list<CocktailResource> Uses stream instead of for loop
+        listResponse = response.getDrinks().stream().map(
+
+                 dpresponce -> new CocktailResource(
+                        UUID.randomUUID(),
+                         dpresponce.getStrDrink(),
+                         dpresponce.getStrGlass(),
+                         dpresponce.getStrInstructions(),
+                         dpresponce.getStrDrinkThumb(),
+                         Stream.of(
+                                 dpresponce.getStrIngredient1(),
+                                 dpresponce.getStrIngredient2(),
+                                 dpresponce.getStrIngredient3(),
+                                 dpresponce.getStrIngredient4(),
+                                 dpresponce.getStrIngredient5()
+                         ).filter(StringUtils::isNotBlank).collect(Collectors.toList())
+                )
+        ).collect(Collectors.toList());
+        return listResponse;
+    }
+
+
+
+
 }
