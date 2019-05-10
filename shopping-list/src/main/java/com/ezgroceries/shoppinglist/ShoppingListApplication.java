be9d3b93afe5c9647b757ece1149a22ecf021908
jd07987 <kenneth.piron@kbc.be>
2019-05-10 12:01:20 +0200

    finished lab 04 and edited tests

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -3,10 +3,9 @@ package com.ezgroceries.shoppinglist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@ComponentScan("com.ezgroceries.shoppinglist.database")
 @EnableFeignClients
 public class ShoppingListApplication {
 
--- shopping-list/src/test/java/com/ezgroceries/shoppinglist/controller/CocktailControllerTest.java
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/controller/CocktailControllerTest.java
@@ -1,19 +1,27 @@
 package com.ezgroceries.shoppinglist.controller;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.ezgroceries.shoppinglist.database.CocktailDBClient;
+import com.ezgroceries.shoppinglist.database.CocktailDBResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -23,11 +31,18 @@ public class CocktailControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private CocktailDBClient cocktailDBClient;
+
+
     @Test
     public void getAccountsTest() throws Exception {
+
+        given(cocktailDBClient.searchCocktails("TEST")).willReturn(giveMockedCocktails());
+
         mockMvc
                 .perform(get("/cocktails")
-                        .param("search", "Russian")
+                        .param("search", "TEST")
                         .accept(MediaType.parseMediaType("application/json")))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
@@ -47,8 +62,40 @@ public class CocktailControllerTest {
                 .andExpect(jsonPath("$[1].ingredients[0]").value("Tequila"))
                 .andExpect(jsonPath("$[1].ingredients[1]").value("Blue Curacao"))
                 .andExpect(jsonPath("$[1].ingredients[2]").value("Lime juice"))
-                .andExpect(jsonPath("$[1].ingredients[3]").value("Salt"))
-        ;
+                .andExpect(jsonPath("$[1].ingredients[3]").value("Salt"));
+
+        verify(cocktailDBClient).searchCocktails("TEST");
+
+
     }
+
+
+    private CocktailDBResponse giveMockedCocktails() {
+        List<CocktailDBResponse.DrinkResource> drinks = new ArrayList<>();
+        CocktailDBResponse.DrinkResource drinkResource = new CocktailDBResponse.DrinkResource();
+        drinkResource.setStrDrink("Margerita");
+        drinkResource.setStrGlass("Cocktail glass");
+        drinkResource.setStrInstructions("Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten..");
+        drinkResource.setStrDrinkThumb("https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg");
+        drinkResource.setStrIngredient1("Tequila");
+        drinkResource.setStrIngredient2("Triple sec");
+        drinkResource.setStrIngredient3("Lime juice");
+        drinkResource.setStrIngredient4("Salt");
+        drinks.add(drinkResource);
+        drinkResource = new CocktailDBResponse.DrinkResource();
+        drinkResource.setStrDrink("Blue Margerita");
+        drinkResource.setStrGlass("Cocktail glass");
+        drinkResource.setStrInstructions("Rub rim of cocktail glass with lime juice. Dip rim in coarse salt..");
+        drinkResource.setStrDrinkThumb("https://www.thecocktaildb.com/images/media/drink/qtvvyq1439905913.jpg");
+        drinkResource.setStrIngredient1("Tequila");
+        drinkResource.setStrIngredient2("Blue Curacao");
+        drinkResource.setStrIngredient3("Lime juice");
+        drinkResource.setStrIngredient4("Salt");
+        drinks.add(drinkResource);
+        CocktailDBResponse cocktailDBResponse = new CocktailDBResponse();
+        cocktailDBResponse.setDrinks(drinks);
+        return cocktailDBResponse;
+    }
+
 
 }
