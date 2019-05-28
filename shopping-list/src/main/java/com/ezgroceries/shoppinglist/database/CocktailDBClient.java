779b6a75b0d6516fcb420f8ba13777619778da5f
jd07987 <kenneth.piron@kbc.be>
2019-05-28 11:43:22 +0200

    lab09 code

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/database/CocktailDBClient.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/database/CocktailDBClient.java
@@ -1,16 +1,43 @@
 package com.ezgroceries.shoppinglist.database;
 
+import com.ezgroceries.shoppinglist.entities.CocktailEntity;
+import com.ezgroceries.shoppinglist.repositories.CocktailRepository;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @Component
-@FeignClient(name = "cocktailDBClient", url = "https://www.thecocktaildb.com/api/json/v1/1")
+@FeignClient(name = "cocktailDBClient", url = "https://www.thecocktaildb.com/api/json/v1/1", fallback = CocktailDBClient.CocktailDBClientFallback.class)
 public interface CocktailDBClient {
 
     @GetMapping(value = "search.php")
     CocktailDBResponse searchCocktails(@RequestParam("s") String search);
 
+
+    @Component
+    class CocktailDBClientFallback implements CocktailDBClient {
+
+        private final CocktailRepository cocktailRepository;
+
+        public CocktailDBClientFallback(CocktailRepository cocktailRepository) {
+            this.cocktailRepository = cocktailRepository;
+        }
+
+        @Override
+        public CocktailDBResponse searchCocktails(String search) {
+            CocktailDBResponse cocktailDBResponse = new CocktailDBResponse();
+            CocktailDBResponse.DrinkResource drink = new CocktailDBResponse.DrinkResource();
+            drink.setStrDrink("fallbacktestDrink");
+            List<CocktailDBResponse.DrinkResource> list = new ArrayList<>();
+            list.add(drink);
+            cocktailDBResponse.setDrinks(list);
+            return cocktailDBResponse;
+        }
+    }
 }
--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/repositories/CocktailRepository.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/repositories/CocktailRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface CocktailRepository extends CrudRepository<CocktailEntity, UUID> {
     List<CocktailEntity> findByIdDrinkIn(List<String> cocktailIds);
     List<CocktailEntity> findAllById(List<UUID> cocktailIds);
+    List<CocktailEntity> findByNameContainingIgnoreCase(String search);
 }
